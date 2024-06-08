1d977d63bb84e32055d7edbaab3fc7982fecd440
Sushrut Bawanthade <thehonoredone1@gmail.com>
2024-06-09 04:27:59 +0530

    user management complete

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+
+
+// npm install connect-flash
+const flash = require('connect-flash');
+const session = require('express-session');
 const connectDB = require('./server/config/db')
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// npm install connect-flash
-const flash = require('connect-flash');
 
-const session = require('express-session');
 // connecct to database
 connectDB();
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
+
 
 app.use(express.static('public'));
 // Express Session
@@ -30,6 +37,7 @@ app.use(
   })
 );
 
+
 // Flash Messages
 app.use(flash({ sessionKeyName: 'flashMessage' }));
 
@@ -37,14 +45,6 @@ app.use(flash({ sessionKeyName: 'flashMessage' }));
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  const locals = {
-    title: "Nodejs",
-    description: "Free NodeJs User Management System"
-  }
-  res.render('index', locals);
-});
 
 // Routes
 app.use('/', require('./server/routes/customer'))
